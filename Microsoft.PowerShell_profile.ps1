e542121ffee293f8689df1e9bd7e4bccd2428a1e
Cole Dapprich <cdsoftworks@gmail.com>
2026-02-05 19:26:26 -0600

    fix: update PS profile
    * replace Starship with oh-my-posh and Spaceship
    * add chocolatey config
    * minor updates

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -17,12 +17,23 @@ function gca { git commit -a }
 function gcf { git commit -m "fixup" }
 function gcaf { git commit -a -m "fixup" }
 function gri2 { git rebase -i HEAD~2 }
-function wttr { curl v2.wttr.in/75082 }
+function gri3 { git rebase -i HEAD~3 }
+function wttr { curl v2.wttr.in/75082?u }
 function wttr-moon { curl www.wttr.in/moon }
 function pwsh { . 'C:\Program Files\PowerShell\7\pwsh.exe'}
 function firefox { . 'C:\Program Files\Mozilla Firefox\firefox.exe' }
 function chrome { . 'C:\Program Files\Google\Chrome\Application\chrome.exe' }
 function gx { . 'C:\Users\cdapprich\AppData\Local\Programs\Opera GX\launcher.exe' }
 
-# starship prompt
-Invoke-Expression (&starship init powershell)
+# oh-my-posh prompt
+oh-my-posh init pwsh --config 'spaceship' | Invoke-Expression
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
